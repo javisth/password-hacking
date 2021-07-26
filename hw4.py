adec123ce3cba48900e03be72760d3742f541eff
javisth <javisth@gmail.com>
2021-07-27 02:35:16 +0530

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,39 @@
+import bcrypt
+import time
+
+fn = open("/Users/javisth/Documents/CS260/student_pwd.txt", "r")
+letters = "abcdefghijklmnopqrstuvwxyz"
+arr = []
+arr2 = []
+newarr = []
+
+for i in letters:
+    for j in letters:
+        passwd = i+j
+        passwd = passwd.encode()
+        arr.append(passwd)
+
+newarr = list(arr)
+
+for line in fn:
+    check = line.partition(',')[2]
+    check = check.rstrip()
+    check = check.encode()
+    arr2.append(check)
+
+while not len(arr2) == 0:
+    check = arr2.pop()
+    arr = list(newarr)  # everytime i pop a password, i reset my array elements
+    while not len(arr) == 0:
+        start = time.time()
+        x = arr.pop()
+        if bcrypt.checkpw(x, check) == True:
+            print(check)
+            print(x)
+            end = time.time()
+            print(start)
+            print(end)
+
+
+
+                
